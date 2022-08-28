cae8e5e4207aeb8b32837166908391420f6455a6
Francesco <emailperlascuolamia@gmail.com>
2022-08-28 20:10:48 +0200

    Changed database port

--- inc/config.php
+++ inc/config.php
@@ -3,6 +3,5 @@ define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "PwNuova2!");
 define("DB_DATABASE_NAME", "shop");
-define("DB_PORT", 41573)
-// port at work: 41573
+define("DB_PORT", 3306)
 ?>
